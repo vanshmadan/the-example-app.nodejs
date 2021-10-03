8deba7b5ed7cbe115ff7aafebf4fd05223efbe56
Vansh Madan <31353777+vanshmadan@users.noreply.github.com>
2021-10-03 23:41:36 +0530

    Create application_start.sh

---
+++ scripts/application_start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# give permission to the files inside /secure_docs directory
+
+sudo chmod -R 777 /home/ubuntu/the-example-app.nodejs
+
+# navigate into current working directory
+
+cd /home/ubuntu/the-example-app.nodejs
+
+# install node modules
+
+npm install
+
+# start our node app in the background using pm2
+
+sudo npm run start:dev
